9ecffc43782361dbe242fa090fa8c5512af2dfe2
Finn <wufeiyang_gtiit@163.com>
2026-04-13 18:49:14 -0400

    Delete ACFG-BEV-Fusion directory

--- ACFG-BEV-Fusion/pcdet/models/backbones_2d/fuser/__init__.py
+++
@@ -1,9 +0,0 @@
-# pcdet/models/backbones_2d/fuser/__init__.py
-
-from .acfg_fuser import ACFGGate
-from .acfg_fuser_gt import ACFGGateGT
-
-__all__ = {
-    "ACFGGate": ACFGGate,
-    "ACFGGateGT": ACFGGateGT,
-}
